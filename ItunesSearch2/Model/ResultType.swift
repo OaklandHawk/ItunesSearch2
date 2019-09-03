1b16a3653e425caf095748dc15ebb07a57e680fb
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-03 15:14:29 -0700

    Added ResultType with enum for software, musicTrack, and movie

---
+++ ItunesSearch2/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  ItunesSearch2
+//
+//  Created by Taylor Lyles on 9/3/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+	case software
+	case musicTrack
+	case movie
+}
