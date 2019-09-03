360342720c1cc6bdf63d47ad77ff08dce9d96c13
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-03 15:21:59 -0700

    Added SearchResultController + baseURL property

---
+++ ItunesSearch2/Controller/SearchResultController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultController.swift
+//  ItunesSearch2
+//
+//  Created by Taylor Lyles on 9/3/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+	
+	let baseURL = URL(string: "https://itunes.apple.com/search")!
+}
