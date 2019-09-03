0cc15be431f166696c2a11b2efa2466a8c6ffe2e
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-03 15:03:31 -0700

    Added CodingKey so the properties will match with JSON

--- ItunesSearch2/Model/SearchResult.swift
+++ ItunesSearch2/Model/SearchResult.swift
@@ -11,4 +11,10 @@ import Foundation
 struct SearchResult: Codable {
 	let title: String
 	let creator: String
+	
+	enum CodingKeys: String, CodingKey {
+		case title = "trackName"
+		case creator = "artistName"
+	}
 }
+
