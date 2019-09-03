4ed78d82f8ee85dd4824bc56d730986c88b2b1c3
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-03 15:01:01 -0700

    Added SearchResult with title and creator properties

---
+++ ItunesSearch2/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  ItunesSearch2
+//
+//  Created by Taylor Lyles on 9/3/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+	let title: String
+	let creator: String
+}
